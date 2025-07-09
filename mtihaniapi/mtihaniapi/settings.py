3a3e22a85aec6a0d99bed4d09c98da3d71a88db5
melaniefayne <melaniefayne33@gmail.com>
2025-07-09 11:20:04 +0300

    Allow all hosts

--- mtihaniapi/mtihaniapi/settings.py
+++ mtihaniapi/mtihaniapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4-i$7*8gtkg#p=t0q$abtsbd&1(k4vs#%g9@d#)&*^^)lm2u5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["melaniefayne.pythonanywhere.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
